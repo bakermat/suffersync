144946810e6a85b25c8bcdc0e266d5ccb1ea7a44
Patrick Peeters <patrickpeeters@outlook.com>
2021-11-20 13:47:50 +0100

    Simplified login

--- README.md
+++ README.md
@@ -5,13 +5,8 @@ SufferSync gets upcoming workouts from your Wahoo SYSTM training plan and syncs 
 ## Getting Started
 - Make sure you have Python 3 with the `requests` and `dateutil` packages. You can install these as follows:
     - `pip install requests python-dateutil`
-- Get your Wahoo SYSTM HTTP Authorization token:
-    - Open your Developer Tools in the Chrome browser, go to https://systm.wahoofitness.com and login. 
-    - Click on the Network tab in the Developer Tools, find a `graphql` entry and click on the Headers tab.
-    - Find an `authorization: Bearer ...` field, copy **only** the (really long) string after `Bearer`.
-    - For example, it might be `authorization: Bearer e9aFylZcMg0IiemLad...` (and more), copy the `e9a` part till the end, don't include `authorization: Bearer`.
 - Get your [intervals.icu](https://intervals.icu) API key on your [account](https://intervals.icu/settings) page.
-- Update the Python script with your Wahoo SYSTM token, the dates that you want to get the activities for and your intervals.icu athlete id & API key.
+- Update the Python script with your Wahoo SYSTM username & password, the dates that you want to get the activities for and your intervals.icu athlete id & API key.
 - Run the app with `python3 ./suffersync.py`
 
 ## Caveats
--- suffersync.py
+++ suffersync.py
@@ -11,13 +11,79 @@ from base64 import b64encode
 # Change these to your own Wahoo SYSTM credentials & intervals.icu                                     #
 # Setup the dates you want to get the workouts for, only future workouts will be sent to intervals.icu #
 ########################################################################################################
-SYSTM_TOKEN = 'xxx'
-INTERVALS_ICU_ID = "i00000"
-INTERVALS_ICU_APIKEY = "xxx"
+SYSTM_USERNAME = 'your_systm_username'
+SYSTM_PASSWORD = 'your_systm_password'
 START_DATE = "2021-11-01T00:00:00.000Z"
 END_DATE = "2021-12-31T23:59:59.999Z"
+INTERVALS_ICU_ID = "i00000"
+INTERVALS_ICU_APIKEY = "xxxxxxxxxxxxx"
+
 
 # Don't change anything below this line
+def get_systm_token(url, username, password):
+    payload = json.dumps({
+        "operationName": "Login",
+        "variables": {
+            "appInformation": {
+                "platform": "web",
+                "version": "7.12.0-web.2141",
+                "installId": "F215B34567B35AC815329A53A2B696E5"
+            },
+            "username": username,
+            "password": password
+        },
+        "query": "mutation Login($appInformation: AppInformation!, $username: String!, $password: String!) { loginUser(appInformation: $appInformation, username: $username, password: $password) { status message user { ...User_fragment __typename } token failureId __typename }}fragment User_fragment on User { id fullName firstName lastName email gender birthday weightKg heightCm createdAt metric emailSharingOn legacyThresholdPower wahooId wheelSize { name id __typename } updatedAt profiles { riderProfile { ...UserProfile_fragment __typename } __typename } connectedServices { name __typename } timeZone onboardingProgress { complete completedSteps __typename } subscription { validUntil trialAvailable __typename } avatar { url original { url __typename } square200x200 { url __typename } square256x256 { url __typename } thumb { url __typename } __typename } onboardingComplete createdWithAppInformation { version platform __typename } __typename}fragment UserProfile_fragment on UserProfile { nm ac map ftp lthr cadenceThreshold riderTypeInfo { name icon iconSmall systmIcon description __typename } riderWeaknessInfo { name __typename } recommended { nm { value activity __typename } ac { value activity __typename } map { value activity __typename } ftp { value activity __typename } __typename } __typename}"
+    })
+
+    headers = {'Content-Type': 'application/json'}
+
+    response = call_api(url, headers, payload)
+    response_json = response.json()
+    token = response_json['data']['loginUser']['token']
+    return token
+
+
+def get_systm_workouts(url, token, start_date, end_date):
+    payload = json.dumps({
+        "operationName": "GetUserPlansRange",
+        "variables": {
+            "startDate": start_date,
+            "endDate": end_date,
+            "queryParams": {
+                "limit": 1000
+            }
+        },
+        "query": "query GetUserPlansRange($startDate: Date, $endDate: Date, $queryParams: QueryParams) { userPlan(startDate: $startDate, endDate: $endDate, queryParams: $queryParams) { ...UserPlanItem_fragment __typename }}fragment UserPlanItem_fragment on UserPlanItem { day plannedDate rank agendaId status type appliedTimeZone completionData { name date activityId durationSeconds style deleted __typename } prospects { type name compatibility description style intensity { master nm ac map ftp __typename } trainerSetting { mode level __typename } plannedDuration durationType metrics { ratings { nm ac map ftp __typename } __typename } contentId workoutId notes fourDPWorkoutGraph { time value type __typename } __typename } plan { id name color deleted durationDays startDate endDate addons level subcategory weakness description category grouping option uniqueToPlan type progression planDescription volume __typename } __typename}"
+    })
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+
+    # Get workouts from Wahoo SYSTM plan
+    response = call_api(url, headers, payload).json()
+    return response
+
+
+def get_systm_workout(url, token, workout_id):
+    payload = json.dumps({
+        "operationName": "GetWorkouts",
+        "variables": {
+            "id": workout_id
+        },
+        "query": "query GetWorkouts($id: ID) {workouts(id: $id) { id sortOrder sport stampImage bannerImage bestFor equipment { name description thumbnail __typename } details shortDescription level durationSeconds name triggers featuredRaces { name thumbnail darkBackgroundThumbnail __typename } metrics { intensityFactor tss ratings { nm ac map ftp __typename } __typename } brand nonAppWorkout notes tags imperatives { userSettings { birthday gender weight __typename } __typename } __typename}}"
+    })
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+
+    response = call_api(url, headers, payload).text
+    return response
+
+
 def call_api(url, headers, payload):
     try:
         response = requests.post(url, headers=headers, data=payload)
@@ -44,35 +110,20 @@ def clean_workout(workout):
 def main():
     SYSTM_URL = "https://api.thesufferfest.com/graphql"
 
-    payload = json.dumps({
-        "operationName": "GetUserPlansRange",
-        "variables": {
-            "startDate": f'{START_DATE}',
-            "endDate": f'{END_DATE}',
-            "queryParams": {
-                "limit": 1000
-            }
-        },
-        "query": "query GetUserPlansRange($startDate: Date, $endDate: Date, $queryParams: QueryParams) { userPlan(startDate: $startDate, endDate: $endDate, queryParams: $queryParams) { ...UserPlanItem_fragment __typename }}fragment UserPlanItem_fragment on UserPlanItem { day plannedDate rank agendaId status type appliedTimeZone completionData { name date activityId durationSeconds style deleted __typename } prospects { type name compatibility description style intensity { master nm ac map ftp __typename } trainerSetting { mode level __typename } plannedDuration durationType metrics { ratings { nm ac map ftp __typename } __typename } contentId workoutId notes fourDPWorkoutGraph { time value type __typename } __typename } plan { id name color deleted durationDays startDate endDate addons level subcategory weakness description category grouping option uniqueToPlan type progression planDescription volume __typename } __typename}"
-    })
+    # Get Wahoo SYSTM auth token
+    systm_token = get_systm_token(SYSTM_URL, SYSTM_USERNAME, SYSTM_PASSWORD)
 
-    systm_headers = {
-        'Authorization': f'Bearer {SYSTM_TOKEN}',
-        'Content-Type': 'application/json'
-    }
+    # Get Wahoo SYSTM workouts from training plan
+    workouts = get_systm_workouts(SYSTM_URL, systm_token, START_DATE, END_DATE)
 
-    # Get workouts fro Wahoo SYSTM plan
-    workouts = call_api(SYSTM_URL, systm_headers, payload).json()
-
-    # Even with errors, response.status_code = 200 so catching errors this way.
+    # Even with errors, response.status_code comes back as 200 so catching errors this way.
     if 'errors' in workouts:
         print(f'Wahoo SYSTM Error: {workouts["errors"][0]["message"]}')
         sys.exit(1)
 
     workouts = workouts['data']['userPlan']
-    workouts_list = []
 
-    # For each item, make sure there's a "plannedDate" field to avoid bogus entries.
+    # For each workout, make sure there's a "plannedDate" field to avoid bogus entries.
     for item in workouts:
         if item['plannedDate']:
             # Get plannedDate, convert to UTC DateTime and then to local timezone
@@ -91,25 +142,18 @@ def main():
 
             try:
                 workout_id = item['prospects'][0]['workoutId']
-                workout_details = {"workout_id": workout_id, "filename": filename}
-                workouts_list.append(workout_details)
             except Exception as err:
                 print(f'Error: {err}')
 
+            # Get specific workout
+            workout_detail = get_systm_workout(SYSTM_URL, systm_token, workout_id)
+
+            # Create .zwo files with workout details
             filename_zwo = f'./zwo/{filename}.zwo'
-
-            payload = json.dumps({
-                "operationName": "GetWorkouts",
-                "variables": {
-                    "id": workout_id
-                },
-                "query": "query GetWorkouts($id: ID) {workouts(id: $id) { id sortOrder sport stampImage bannerImage bestFor equipment { name description thumbnail __typename } details shortDescription level durationSeconds name triggers featuredRaces { name thumbnail darkBackgroundThumbnail __typename } metrics { intensityFactor tss ratings { nm ac map ftp __typename } __typename } brand nonAppWorkout notes tags imperatives { userSettings { birthday gender weight __typename } __typename } __typename}}"
-            })
-
-            workout_detail = call_api(SYSTM_URL, systm_headers, payload).text
+            os.makedirs(os.path.dirname(filename_zwo), exist_ok=True)
 
             try:
-                # Workout details is not clean JSON, so use clean_workout() before loading as JSON
+                # Workout details are not clean JSON, so use clean_workout() before loading as JSON
                 workout_detail = clean_workout(workout_detail)
                 workout_json = json.loads(workout_detail)
                 sport = workout_json['data']['workouts'][0]['sport']
@@ -118,20 +162,19 @@ def main():
                 if sport == 'Yoga':
                     continue
 
+                # 'triggers' contains the FTP values for the workout
                 workout_json = workout_json['data']['workouts'][0]['triggers']
 
-                interval_name = ''
-                os.makedirs(os.path.dirname(filename_zwo), exist_ok=True)
                 f = open(filename_zwo, "a")
                 if not workout_json:
                     f.write('No workout data found.')
                     f.close()
                 else:
-                    text = f"""
+                    text = r"""
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <workout_file>
     <author></author>
-    <name>{interval_name}</name>
+    <name></name>
     <description></description>
     <sportType>bike</sportType>
     <tags/>
@@ -139,12 +182,7 @@ def main():
                     f.write(text)
 
                     for interval in range(len(workout_json)):
-                        interval_name_counter = 0
                         for tracks in range(len(workout_json[interval]['tracks'])):
-                            if 'name' in workout_json[interval]:
-                                interval_name = workout_json[interval]['name']
-                            if interval_name_counter == 0:
-                                interval_name_counter += 1
                             for item in workout_json[interval]['tracks'][tracks]['objects']:
                                 seconds = int(item['size'] / 1000)
                                 if 'ftp' in item['parameters']:
