a9ecb0db939e91b7cb525aa71d9bdfb7d4d4d604
Patrick Peeters <12652989+bakermat@users.noreply.github.com>
2022-01-19 16:53:17 +0100

    Added GitHub Actions for publishing to PyPi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='suffersync',
-    version='1.2.0-dev',
+    version='1.2.0',
     description='Syncs workouts from Wahoo SYSTM to intervals.icu',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
